096f34a6c9ae922503fbadd30a11ee9e6efcd994
KhalilSelyan <khalil@leodrive.ai>
2025-01-01 23:37:29 +0300

    relatively better header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import {
+		DropdownMenu,
+		DropdownMenuContent,
+		DropdownMenuItem,
+		DropdownMenuTrigger,
+	} from "$lib/components/ui/dropdown-menu";
+	import ThemeToggler from "@/components/ThemeToggler.svelte";
+	import { Button } from "@/components/ui/button";
+	import { route, routes } from "@/ROUTES";
+	import { ChevronDown } from "lucide-svelte";
+</script>
+
+<div class="flex w-full items-center justify-between gap-4 px-4">
+	<Button variant="ghost" href={route("/")}>Home</Button>
+	<div class="flex items-center gap-2">
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<Button variant="ghost" class="gap-1">
+					Menu
+					<ChevronDown />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				{#each routes.filter((r) => r !== "/") as link}
+					<DropdownMenuItem>
+						<Button variant="ghost" href={route(link)} class="w-full justify-start">
+							{link}
+						</Button>
+					</DropdownMenuItem>
+				{/each}
+			</DropdownMenuContent>
+		</DropdownMenu>
+		<ThemeToggler />
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,14 @@
 <script lang="ts">
-	import { route, routes } from "@/ROUTES";
 	import { ModeWatcher } from "mode-watcher";
 	import "../app.css";
+	import Header from "@/components/Header.svelte";
 
-	import ThemeToggler from "@/components/ThemeToggler.svelte";
-	import { Button } from "@/components/ui/button";
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
 
-<main>
-	<div class="flex gap-4">
-		{#each routes as link}
-			<Button variant="link" href={route(link)}>{link}</Button>
-		{/each}
-		<ThemeToggler />
-	</div>
-
+<main class="container">
+	<Header />
 	{@render children()}
 </main>
