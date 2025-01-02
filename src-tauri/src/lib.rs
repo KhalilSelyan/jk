31ac195a82f71866552c4ce0de1b8c46fce127f3
KhalilSelyan <khalil@leodrive.ai>
2025-01-02 03:31:33 +0300

    add ctrlq quitapp

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -69,6 +69,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+	import Header from "@/components/Header.svelte";
+	import { exit } from "@tauri-apps/plugin-process";
 	import { ModeWatcher } from "mode-watcher";
 	import "../app.css";
-	import Header from "@/components/Header.svelte";
-
 	let { children } = $props();
+
+	$effect(() => {
+		const handleKeyDown = async (e: KeyboardEvent) => {
+			if (e.ctrlKey && !e.shiftKey && e.key === "q") {
+				// Close window using Tauri API
+				await exit(0);
+			}
+		};
+
+		window.addEventListener("keydown", handleKeyDown);
+		return () => window.removeEventListener("keydown", handleKeyDown);
+	});
 </script>
 
 <ModeWatcher />
